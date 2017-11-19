a48ca707ed1552eb41e5d8185124732d014c8422
Amrit Gill <amgill1234@gmail.com>
2017-11-19 14:21:33 -0500

    More refactor

--- chef_utils/chef_upload.sh
+++ chef_utils/chef_upload.sh
@@ -2,7 +2,6 @@
 
 CHEF="$HOME/chef-repo"
 ENV='all_envs.txt'
-COOKBOOKS='required_cookbooks.txt'
 ROLES='roles/*.rb'
 DATA_BAGS="$CHEF/data_bags"
 
@@ -22,15 +21,10 @@ list_on_chef_server(){
 
 
 
-upload_required_cookbooks(){
+upload_all_cookbooks(){
 	echo "=============== Uploading required cookbooks ==============="
-	for i in $(cat $COOKBOOKS); do
-		if [ ! -d cookbooks/$i ]
-		then
-			echo "$i directory does not exists!!!"
-		else
-			knife cookbook upload $i
-		fi
+	for x in $(ls "$CHEF/cookbooks"); do
+		knife cookbook upload $x
 	done
 echo "=============== All cookbooks successfully uploaded! ==============="
 }
@@ -63,7 +57,7 @@ cd $CHEF
 pwd
 export PATH=/bin:/usr/bin:/usr/local/bin:/sbin:/usr/sbin
 upload_db
-upload_required_cookbooks
+upload_all_cookbooks
 upload_all_envs
 upload_all_roles
 
